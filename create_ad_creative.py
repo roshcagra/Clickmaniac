88195db7d165d188bd804bc024772ea2aeee5989
roshcagra <roshan.agrawal95@gmail.com>
2017-03-06 15:52:24 -0800

    Finished

--- create_ad_creative.py
+++ create_ad_creative.py
@@ -1,25 +1,18 @@
 from facebookads.api import FacebookAdsApi
 from facebookads.adobjects.adcreative import AdCreative
-from facebookads.adobjects.adcreativelinkdata import AdCreativeLinkData
-from facebookads.adobjects.adcreativeobjectstoryspec \
-    import AdCreativeObjectStorySpec
 
 from constants import *
 
 FacebookAdsApi.init(my_app_id, my_app_secret, my_access_token)
 
-link_data = AdCreativeLinkData()
-link_data[AdCreativeLinkData.Field.message] = "Everyone needs a home! Click 'Like' and help the Pasadena Humane Society!"
-link_data[AdCreativeLinkData.Field.link] = 'http://www.facebook.com/caltech.clickmaniac'
-link_data[AdCreativeLinkData.Field.image_hash] = puppy_image_hash
-
-object_story_spec = AdCreativeObjectStorySpec()
-object_story_spec[AdCreativeObjectStorySpec.Field.page_id] = 103185246428488
-object_story_spec[AdCreativeObjectStorySpec.Field.link_data] = link_data
-
 creative = AdCreative(parent_id=my_account_id)
-creative[AdCreative.Field.name] = 'Puppies'
-creative[AdCreative.Field.object_story_spec] = object_story_spec
+creative[AdCreative.Field.name] = 'Puppies Ad'
+creative[AdCreative.Field.title] = 'Puppies'
+creative[AdCreative.Field.body] = "Everyone needs a home! Click 'Like' and help the Pasadena Humane Society!"
+creative[AdCreative.Field.object_id] = '103185246428488'
+creative[AdCreative.Field.object_type] = AdCreative.ObjectType.page
+creative[AdCreative.Field.image_hash] = puppy_image_hash
+
 creative.remote_create()
 
 print(creative)
